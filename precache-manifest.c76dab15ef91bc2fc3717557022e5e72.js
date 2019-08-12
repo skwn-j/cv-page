c2511802e980297e75a7fc8b08d52495dac780c7
Seokweon Jung <minimus@snu.ac.kr>
2019-08-12 13:27:36 +0900

    Updates

---
+++ precache-manifest.c76dab15ef91bc2fc3717557022e5e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b86b373890bce8a07940d201a16a25",
+    "url": "/cv-page/index.html"
+  },
+  {
+    "revision": "5fc0c51fccfbe5e252e4",
+    "url": "/cv-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1a7f2569fcbecb771d47",
+    "url": "/cv-page/static/js/2.9c8c5acb.chunk.js"
+  },
+  {
+    "revision": "5fc0c51fccfbe5e252e4",
+    "url": "/cv-page/static/js/main.2d4dd1bf.chunk.js"
+  },
+  {
+    "revision": "e03914f329df02e7c427",
+    "url": "/cv-page/static/js/runtime~main.8146cbdc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
