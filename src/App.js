1c1a2ee17437e0962dec2ab07c8130aaef9303c7
Seokweon Jung <minimus@snu.ac.kr>
2019-08-13 15:29:26 +0900

    coco

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 
 const brushcoco = './brushcoco.jpg';
 const normalcoco = './normalcoco.jpg';
+
+let targetcoco = normalcoco;
+
+const handleClick = () => {
+  if(targetcoco === brushcoco) targetcoco = normalcoco;
+  else targetcoco = brushcoco;
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={brushcoco} className="coco" alt="coco" width={300} height={300} />
+        <img src={targetcoco} className="coco" alt="coco" width={300} height={300} onClick={handleClick}/>
       </header>
       <div>
         
